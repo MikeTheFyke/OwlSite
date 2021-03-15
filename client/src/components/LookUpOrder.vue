b592e0eb67886561cbcd60c31035dee05f34b470
MikeTheFyke <mikefyke@hotmail.com>
2021-03-14 23:23:44 -0400

    Adjsuting Order list container

--- client/src/components/LookUpOrder.vue
+++ client/src/components/LookUpOrder.vue
@@ -16,6 +16,43 @@
             </form>
         </div>
     </div>
+
+          <div id="receiptModal" class="modal" >
+              <div class="modal-content">
+                    <span class="close" v-on:click="closeReceiptModal">&times;</span>
+                    <h1 id="modalHeaderText">Your Weight Receipt</h1>
+                      <div id="receiptDataContainer">
+                          <table id='receiptItemsTable'>
+                              <tr><th>Carrier Name</th><th>Destination City</th><th>Weight</th><th>Cost</th><th>Customer Name</th></tr>
+                              <tbody id="newItems"></tbody>
+                              <tr>
+                              <tr v-for="(order, index) in newOrder" 
+                                  v-bind:item="order" 
+                                  v-bind:index="index" 
+                                  v-bind:key="order._id"
+                                  
+                                  >
+                                <td>{{ order.carrierName }} </td>
+                                <!-- <td>{{ order.serviceType }} </td> -->
+                                <td>{{ order.destinationCity }} </td>
+                                <td>{{ order.weight }} </td>
+                                <!-- <td>{{ order.numberOfSkids }} </td> -->
+                                <!-- <td>{{ order.numberOfSpots }} </td> -->
+                                <td>{{ order.cost }} </td>
+                                <td>{{ order.customerName }} </td>
+                              </tr>
+                          </table>
+                      </div>
+                        <!-- <div id="receiptTotals">
+                            <td class="receiptTotalsItem" id="receiptTotalSpots">Spots</td>
+                            <td class="receiptTotalsItem" id="receiptTotalHandling">Handling Units</td>
+                            <td class="receiptTotalsItem" id="receiptTotalPieces">Piece Count</td>
+                            <td class="receiptTotalsItem" id="receiptCalculatedWeight"> 0 </td>
+                            <td class="receiptTotalsItem" id="receiptTotalWeight"> Total Weight </td>
+                        </div> -->
+              </div>  
+          </div>
+
   </div>
 </template>
 
@@ -27,7 +64,7 @@ export default {
   name: 'OrderSearch',
     data() {
         return{
-            newCarrier : [],
+            newOrder : [],
             error : '',
             carrierName : '',
             serviceType: '',
@@ -144,6 +181,86 @@ button{
     border-radius: 10px;
     color: white;
     outline: none;
+}
+
+/* Order List */
+
+#receiptModal{
+    display: initial;
+    position: absolute;
+    z-index: 1;
+    left: 0;
+    top: 260px;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: transparent;
+}
+
+.modal-content{
+    background-color: #E26125;
+    margin: 0% auto;
+    padding: 0px 5px 5px 5px;
+    border-radius: 25px;
+    width: 70%;
+    border-color: white;
+    border-width: 5px;
+    border-style: solid;
+}
+
+#receiptDataContainer{
+    background-color: white;
+    border-radius: 15px;
+    width: 100%;
+    margin-top: 10px;
+    overflow: hidden;
+}
+
+td{
+    color: #1947BA;
+    font-size: 24px;
+    text-align: center;
+}
+
+.close{
+    float: right;
+    color: #1947BA;
+    font-size: 32px;
+    font-weight: bold;
+    margin-right: 5px;
+}
+
+.close:hover,
+.close:focus {
+    color: white;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+#modalHeaderText {
+    color: white;
+    width: 300px;
+    margin: 0px auto;
+}
+
+#receiptItemsTable{
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#receiptItemsTable th{
+  width: 25%;
+  background-color: transparent;
+}
+
+#newItems{
+  width: 25%;
+  color: #1947BA;
+  text-align: center;
+}
+
+#receiptItemsTable tr:nth-child(even){
+  background-color: #f2f2f2;
 }
 
 </style>
