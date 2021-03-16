8f7a7a6462fe5c43c7be6dc542141cb0a99a3e53
MikeTheFyke <mikefyke@hotmail.com>
2021-03-15 20:58:53 -0400

    updated orders route logic

--- server/routes/api/orders.js
+++ server/routes/api/orders.js
@@ -3,34 +3,39 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
-// GET Posts
+// GET Orders
 router.get('/', async (req, res) => {
-    const orders = await loadordersCollection();
+    const orders = await loadOrdersCollection();
     res.send(await orders.find({}).toArray());
 })
 
-// // ADD Posts
-// router.post('/', async (req, res) => {
-//     const posts = await loadPostsCollection();
-//     await posts.insertOne({
-//         name: req.body.name,
-//         price: "$0.00",
-//         quantity: req.body.quantity,
-//         createdAt: new Date()
-//     });
-//     res.status(201).send();
-// });
+// ADD Orders
+router.post('/', async (req, res) => {
+    const orders = await loadOrdersCollection();
+    await orders.insertOne({
+        name: req.body.carrierName,
+        serviceType: req.body.serviceType,
+        destinationCity: req.body.destinationCity,
+        weight: req.body.weight,
+        numberSkids: req.body.numberSkids,
+        numberSpots: req.body.numberSpots,
+        cost: req.body.cost,
+        customerName: req.body.customerName,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
 
-// // DELETE Post
-// router.delete('/:id', async (req, res) => {
-//     const posts = await loadPostsCollection();
-//     await posts.deleteOne({_id: new mongodb.ObjectID (req.params.id)})
-//     console.log("Deleting")
-//     res.status(200).send({});
-// })
+// DELETE Orders
+router.delete('/:id', async (req, res) => {
+    const orders = await loadOrdersCollection();
+    await orders.deleteOne({_id: new mongodb.ObjectID (req.params.id)})
+    console.log("Deleting")
+    res.status(200).send({});
+})
 
 // Connection to MongoDB
-async function loadordersCollection() {
+async function loadOrdersCollection() {
     const client = await mongodb.MongoClient.connect
     ('mongodb+srv://mike_123:mike_123@cluster0.yp0q2.mongodb.net/owl_site?retryWrites=true&w=majority', {
         useNewUrlParser: true,
